fdd2aa60ba4c7488444b2b1027d0a5dd076863aa
Kefyalew <koutlook19@gmail.com>
2025-12-24 14:53:05 +0300

    Setting up Jolly G collection website

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+import './App.css'
+
+function App() {
+   return(
+    <div className='text-2xl font-bold underline text-blue-500 '>
+    
+      <h1> Hello</h1>
+    </div>
+   )
+  
+}
+
+export default App
